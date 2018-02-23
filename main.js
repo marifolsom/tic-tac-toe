697deb1b51ddf653318cb93f37a4f126b60c970e
marifolsom <mfolsom1@fordham.edu>
2018-02-23 16:51:32 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// 9 boxes for tic-tac-toe
+// Assign a number to each of the boxes
+// Make event listeners for each box?
+// Listen for click and then make background an of x or o
+// or insert big letter X or O
+// User places marker with click
+// Check for win (3 in a row) by comparing against other boxes
+
+var $boxOne = $();
+var $boxTwo = $();
+var $boxThree = $();
+var $boxFour = $();
+var $boxFive = $();
+var $boxSix = $();
+var $boxSeven = $();
+var $boxEight = $();
+var $boxNine = $();
