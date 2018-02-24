0a9815f519f2d81359ddc92ffdb46f49dc30b4f1
marifolsom <mfolsom1@fordham.edu>
2018-02-24 17:49:24 -0500

    created a container div to house all the box divs and added Xs and Os when player clicks

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+console.log('main.js linked!');
+
 // Tic-Tac-Toe game:
   // 9 boxes/turns in a game -- assign a number to each of the boxes
   // Players take turns placing a marker in a box
@@ -12,21 +14,17 @@
 
 
 
-console.log('main.js linked!');
-
-
-
 // Make an array that holds the win states
 var winStates = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
-
-// Make variables for each player's choices
+// Make variables to store each player's choices
 var choicesX = [];
 var choicesO = [];
-// // var choicesX = [2, 5, 7, 4];
-// // var choicesO = [1, 3, 6, 8, 9];
-
 // Make a variable to count number of matches in a winning array
 var counter = 0;
+// Make a variable that counts the amount of times the mouse has been clicked
+var round = 0;
+
+
 
 var checkForWins = function (arr) {
   // Check through the array of winning arrays
@@ -39,9 +37,11 @@ var checkForWins = function (arr) {
       for (var k = 0; k < winStates[i].length; k++) {
         // // console.log to see what every loop is doing
         // // console.log(winStates[i], arr[j], winStates[i][k]);
+        // If the numbers in the winning arrays match a number from the array of the player's choices, add to the counter
         if (winStates[i][k] === arr[j]) {
           counter++;
         }
+        // If three in a row, player wins
         if (counter === 3) {
           alert ('WIN!');
         }
@@ -52,44 +52,63 @@ var checkForWins = function (arr) {
 
 
 
-// Make a variable that counts the amount of times the mouse has been clicked
-var round = 0;
-
 var boxClick = function (evt) {
-  // // console.log to see what box is clicked
+  // // console.log to see which box is clicked
   // // console.log('Clicked ' + evt.target.id);
-  // Add to round count with every click
-  round ++;
   // Turn box name into a number in order to be able to compare to winning arrays
   var index = evt.target.id.charAt(3);
-  // Differentiate between the 2 players (odd = X, even = O), and push into respective arrays
-  if (round % 2 !== 0) {
-    // Push into choicesX
-    choicesX.push(Number(index));
-    console.log('choicesX: ' + choicesX);
-    // Call checkForWins on choicesX
-    checkForWins(choicesX);
+  // If the round is less than 9, then keep checking for wins
+  if (round < 9) {
+    // Differentiate between the 2 players (odd = X, even = O), and push into respective arrays
+    if (round % 2 !== 0) {
+      // Push into choicesX
+      choicesX.push(Number(index));
+      // // console.log to see the choicesX array as it's being made
+      // // console.log('choicesX: ' + choicesX);
+      // Call checkForWins on choicesX
+      checkForWins(choicesX);
+      // Add an X
+      $('#box' + index).text('X');
+    } else {
+      // Push into choicesO
+      choicesO.push(Number(index));
+      // // console.log to see the choicesO array as it's being made
+      // // console.log('choicesO: ' + choicesO);
+      // Call checkForWins on choicesO
+      checkForWins(choicesO);
+      // Add an O
+      $('#box' + index).text('O');
+    }
   } else {
-    // Push into choicesO
-    choicesO.push(Number(index));
-    console.log('choicesO: ' + choicesO);
-    // Call checkForWins on choicesO
-    checkForWins(choicesO);
-  }
-  // Turn off the event listener once a box has been clicked
-  $(evt.target).off();
-  if (round === 9) {
+    // Otherwise, players draw when 9 rounds have passed
     alert ('DRAW!')
   }
+  // Turn off the event listener once a box has been clicked so it can't be clicked again
+  $(evt.target).off();
+  // Add to round count with every box click
+  round ++;
+  console.log(round);
 }
 
 
-// Make a for loop to make 9 divs
+
+// Make a for loop to make 9 divs and add event listeners
 for (var i = 1; i <= 9; i++) {
   // Create the divs with an id
-  var $div = $('body').append('<div id="box' + i + '">' + i + '</div>');
+  var $div = $('body').append('<div class="boxes" id="box' + i + '"></div>');
   // Style each of the divs
-  $('#box' + i).css({'border': '5px solid black', 'width': '150px', 'height': '150px'});
+  $('#box' + i).css({
+    'border': '5px solid black',
+    'width': '150px',
+    'height': '150px',
+    'display': 'inline-block'
+  });
   // Add event listeners to the divs for when each box is clicked
   $('#box' + i).on('click', boxClick);
 }
+
+
+// Add box divs into the container div
+var $container = $('.container').css({'width': '500px', 'margin': '0, auto'});
+var $boxes = $('.boxes');
+$container.append($boxes);
